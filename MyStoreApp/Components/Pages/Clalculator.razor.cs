4c930392482f4ea3cabfcde5bb19bb2d22dc671c
himani204 <np05cp4s240025@iic.edu.np>
2025-12-16 12:11:54 +0545

    Initial commit

---
+++ MyStoreApp/Components/Pages/Clalculator.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+public partial class Calculator
+{
+    protected double num1 = 0;
+    protected double num2 = 0;
+    protected double result = 0;
+
+    protected void Add()
+    {
+        result = num1 + num2;
+    }
+
+    protected void Subtract()
+    {
+        result = num1 - num2;
+    }
+
+    protected void Multiply()
+    {
+        result = num1 * num2;
+    }
+
+    protected void Divide()
+    {
+        if (num2 != 0)
+        {
+            result = num1 / num2;
+        }
+    }
+}
